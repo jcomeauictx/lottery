4a9c38ba088ca6c8137a59116def9b89a00eca37
John Comeau <jc@unternet.net>
2018-04-28 15:48:27 -0700

    apparently that require statement also breaks it EVENTS: cumulative gas used: 42272 currentBlock: 0 lastBlockhash: 0x eth.accounts[0] balance: 5.776730299999999999925e+21 eth.accounts[1] balance: 5100000000000000075 lottery jackpot: 0 lottery balance: 11100000000000000 lottery has apparently selfdestructed

--- jclottery.sol
+++ jclottery.sol
@@ -85,6 +85,8 @@ contract Lottery {
         uint payout;
         bool sent;
         uint funds;
+        emit LogMessage("comparing block.number to currentBlock");
+        require(block.number >= currentBlock);
         if (block.number > currentBlock) {
             lastBlockhash = block.blockhash(currentBlock);
             /* don't check for winners if the pot has fewer than 10 entries,
